8d286adaf11e2dba510d11be7999ca38ad04e0b8
justnotnik <yannik98@yandex.ru>
2023-11-04 20:14:01 +0300

    Реализована базовая версия с загрузкой файлов или zip архива с файлами

---
+++ main.py
@@ -0,0 +1,42 @@
+import zipfile
+
+from fastapi import FastAPI, UploadFile, File
+from typing import Union
+from fastapi.responses import HTMLResponse
+import uvicorn
+import os
+
+app = FastAPI()
+#сюда попадут все файлы
+UPLOAD_DIR = './ups/'
+
+
+@app.get("/")
+def read_root():
+    with open("./static/index.html") as f:
+        data = f.read()
+    return HTMLResponse(content=data)
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/upload-files")
+async def upload_file(file: UploadFile):
+    if file.filename == "":
+        return "error-42"
+    data = file.file
+    path = os.path.join(UPLOAD_DIR, file.filename)
+    with open(path, 'wb') as f:
+        f.write(data.read())
+    if str(file.filename).endswith(".zip"):
+        with zipfile.ZipFile(path, 'r') as zip_ref:
+            zip_ref.extractall(UPLOAD_DIR)
+        os.remove(path)
+    return {"filename": file.filename}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
