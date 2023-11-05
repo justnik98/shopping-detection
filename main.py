733ce603923c8dab5c0e8291c890e7137e232d3f
justnotnik <yannik98@yandex.ru>
2023-11-06 02:06:01 +0300

    Добавлена децимация фреймов и очистка очереди

--- main.py
+++ main.py
@@ -14,14 +14,17 @@ import os
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-#сюда попадут все файлы
+# сюда попадут все файлы
 UPLOAD_DIR = './ups/'
 
 templates = Jinja2Templates(directory="templates")
 
 some_file_path = "./static/test.mp4"
 cap = cv2.VideoCapture("rtsp://127.0.0.1:8554/test.mp4")
-url_rtsp = 'rtsp://127.0.0.1:8554/screenlive'
+
+
+# cap = cv2.VideoCapture("rtsp://127.0.0.1:8554/test.mp4 !queue ! decodebin ! videoconvert ! appsink max-buffers=1 drop=true")
+# url_rtsp = 'rtsp://127.0.0.1:8554/screenlive'
 
 # def iterfile():
 #     cap = cv2.VideoCapture("rtsp://127.0.0.1:8554/screenlive")
@@ -34,37 +37,54 @@ url_rtsp = 'rtsp://127.0.0.1:8554/screenlive'
 #         yield frame
 @app.get("/")
 def read_root(request: Request):
-    #return FileResponse("./static/index.html")
+    # return FileResponse("./static/index.html")
     return templates.TemplateResponse("index.html", {"request": request, "id": 2})
 
-count = 0
+
 @app.get("/video")
 async def video_feed():
+
+
     def iterfile():  #
-        global count
+        count = 0
+        n = 1
         while (cap.isOpened()):
+            # скипаем кадры в буфере (какой чудак придумал стримить по TCP)
+            for i in range(5):
+                cap.grab()
             ret, frame = cap.read()
             count += 1
-            if count % 5 != 0:
+            # тут будем скипать кадры для отправки в модель
+            if count % n != 0:
                 continue
+            else:
+                count -= n
+            time.sleep(0.05)  # Иммитация обработки
             # print(frame)
             # print(type(frame))
-            #time.sleep(0.002)
-            #cv2.imshow('frame', frame)
+            # time.sleep(0.002)
+            # cv2.imshow('frame', frame)
             outputFrame = frame
-            cv2.rectangle(frame, (1240 + random.randint(-200,200),637+ random.randint(-200,200)), (225+ random.randint(-200,200), 897+ random.randint(-200,200)),thickness= 3,color=(0,0,255))
+            cv2.rectangle(frame, (1240 + random.randint(-200, 200), 637 + random.randint(-200, 200)),
+                          (225 + random.randint(-200, 200), 897 + random.randint(-200, 200)), thickness=3,
+                          color=(0, 0, 255))
             (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+
             if not flag:
                 continue
             if cv2.waitKey(20) & 0xFF == ord('q'):
                 break
             yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
                    bytearray(encodedImage) + b'\r\n')
+
             # frame = imutils.resize(frame, width=680)
 
         # with open(some_file_path, mode="rb") as file_like:  #
         #     yield from file_like  #
+
     return StreamingResponse(iterfile(), media_type="multipart/x-mixed-replace;boundary=frame")
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
@@ -82,7 +102,7 @@ async def upload_file(file: UploadFile):
         with zipfile.ZipFile(path, 'r') as zip_ref:
             zip_ref.extractall(UPLOAD_DIR)
         os.remove(path)
-    return RedirectResponse("/", status_code=302)#{"filename": file.filename}
+    return RedirectResponse("/", status_code=302)  # {"filename": file.filename}
 
 
 if __name__ == '__main__':
